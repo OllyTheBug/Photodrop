ec065d83a8b08e7b379b3d8bd601e620f0060623
OllyTheBug <mendelse@oregonstate.edu>
2022-07-19 12:43:50 -0700

    Restructure

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\Olly\\Documents\\instaclone-356500-e4bf652fddeb.json'
+
+# Local imports
+from instaclone.__init__ import app
+from instaclone.views import views
+
+
+
+
+if __name__ == '__main__':
+    
+    # -------------------------------- Load views -------------------------------- #
+    app.register_blueprint(views)
+    app.run(host='localhost', port=8080, debug=True, use_reloader=False)
