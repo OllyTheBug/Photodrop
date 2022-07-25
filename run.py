65766b1cf95c2f2c8d9d13f0faa86b4ae1bff616
OllyTheBug <mendelse@oregonstate.edu>
2022-07-25 14:23:55 -0700

    get username in run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\mendelse\\Documents\\instaclone.json'
+# Get computer user's name
+username = os.getlogin()
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = f'C:\\Users\\{username}\\Documents\\instaclone.json'
 
 # Local imports
 from instaclone.__init__ import app
